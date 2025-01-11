70d38b6cb51737c87307295a5be40884876802c1
Ha-Mundo <trauzzu@gmail.com>
2025-01-11 19:03:50 +0100

    implemented ReservationList client component

---
+++ app/_components/ReservationList.js
@@ -0,0 +1,33 @@
+"use client";
+
+import { deleteReservation } from "@/app/_lib/actions";
+import ReservationCard from "@/app/_components/ReservationCard";
+import { useOptimistic } from "react";
+
+function ReservationList({ bookings }) {
+  const [optimisticBookings, optimisticDelete] = useOptimistic(
+    bookings,
+    (curBookings, bookingId) => {
+      return curBookings.filter(booking => booking.id !== bookingId);
+    }
+  );
+
+  async function handleDelete(bookingId) {
+    optimisticDelete(bookingId); // <- this instant update the optimisticBookings state
+    await deleteReservation(bookingId);
+  }
+
+  return (
+    <ul className="space-y-6">
+      {optimisticBookings.map(booking => (
+        <ReservationCard
+          booking={booking}
+          onDelete={handleDelete}
+          key={booking.id}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default ReservationList;
